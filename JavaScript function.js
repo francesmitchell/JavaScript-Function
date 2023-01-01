3ee664ea3f82d7958e648b2f8be8c6f8a3ee05b1
francesmitchell <121049096+francesmitchell@users.noreply.github.com>
2022-12-31 23:03:36 -0500

    Add files via upload

---
+++ JavaScript function.js
@@ -0,0 +1,11 @@
+var msg = 'Hello World';
+console.log(msg);
+var msg = 'Frances D.L. Mitchell';
+console.log(name);
+var msg = '5'5 ft';
+console.log(height);
+var msg = 'USA';
+console.log(Country);
+alert(name)
+alert(height)
+alert(Country)
